ce1cae8fb798ef4669cb08be7dc62da9da2229fc
Maryam Quadir <maryamquadir@gmail.com>
2014-09-21 13:18:36 -0700

    Added Search Menu

--- src/com/maryamq/imageclient/SearchActivity.java
+++ src/com/maryamq/imageclient/SearchActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 
 public class SearchActivity extends Activity {
 
@@ -11,5 +13,36 @@ public class SearchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+    
+    private boolean triggerSearch(String input) {
+    	if (input == null || input.isEmpty()) {
+    		return false;
+    	}
+    	
+    	return true;
+    }
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	getMenuInflater().inflate(R.menu.search_menu, menu);
+    	MenuItem searchItem = menu.findItem(R.id.action_search);
+    	SearchView searchView = (SearchView) searchItem.getActionView();
+    	searchView.setOnQueryTextListener(new OnQueryTextListener() {
+
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				// TODO Auto-generated method stub
+				return triggerSearch(query);
+			}
+
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				return true;
+			}
+    		
+    	});
+    	return super.onCreateOptionsMenu(menu);
     }
 }
