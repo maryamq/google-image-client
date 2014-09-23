07280b16f475c916de99d6127e583419b93de54c
Maryam Quadir <maryamquadir@gmail.com>
2014-09-23 10:18:50 -0700

    Add Share feature

--- src/com/maryamq/imageclient/Utils.java
+++ src/com/maryamq/imageclient/Utils.java
@@ -1,9 +1,19 @@
 package com.maryamq.imageclient;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Environment;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -11,28 +21,34 @@ import com.squareup.picasso.RequestCreator;
 import com.squareup.picasso.Transformation;
 
 public class Utils {
-	
+
 	public static boolean isNetworkAvailable(Context context) {
-	    ConnectivityManager connectivityManager 
-	          = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-	    return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+		ConnectivityManager connectivityManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager
+				.getActiveNetworkInfo();
+		return activeNetworkInfo != null
+				&& activeNetworkInfo.isConnectedOrConnecting();
 	}
-	
-	public static void loadPhoto(ImageView ivImage, Context context, String url){
+
+	public static void loadPhoto(ImageView ivImage, Context context, String url) {
 		RequestCreator picassoRequest = Picasso.with(context).load(url);
-		picassoRequest.transform(Utils.getImageScaleTransformer(ivImage.getMeasuredWidth(), ivImage.getMeasuredHeight()));
+		picassoRequest.transform(Utils.getImageScaleTransformer(
+				ivImage.getMeasuredWidth(), ivImage.getMeasuredHeight()));
 		picassoRequest.into(ivImage);
 	}
-	
-	public static void loadPhotoForWidth(ImageView ivImage, Context context, String url){
+
+	public static void loadPhotoForWidth(ImageView ivImage, Context context,
+			String url) {
 		RequestCreator picassoRequest = Picasso.with(context).load(url);
-		picassoRequest.transform(Utils.getImageScaleWidthTransformer(ivImage.getWidth()));
+		picassoRequest.transform(Utils.getImageScaleWidthTransformer(ivImage
+				.getWidth()));
 		picassoRequest.into(ivImage);
-		
+
 	}
-	
-	public static Transformation getImageScaleWidthTransformer(final int targetWidth) {
+
+	public static Transformation getImageScaleWidthTransformer(
+			final int targetWidth) {
 		return new Transformation() {
 
 			@Override
@@ -51,7 +67,54 @@ public class Utils {
 			}
 		};
 	}
-	
+
+	public static boolean onShareItem(View v, int imageViewId) {
+		// Get access to bitmap image from view
+		ImageView ivImage = (ImageView) v.findViewById(imageViewId);
+		// Get access to the URI for the bitmap
+		Uri bmpUri = getLocalBitmapUri(ivImage);
+		if (bmpUri != null) {
+			// Construct a ShareIntent with link to image
+			Intent shareIntent = new Intent();
+			shareIntent.setAction(Intent.ACTION_SEND);
+			shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+			shareIntent.setType("image/*");
+			// Launch sharing dialog for image
+			v.getContext().startActivity(
+					Intent.createChooser(shareIntent, "Share Image"));
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static Uri getLocalBitmapUri(ImageView imageView) {
+		// Extract Bitmap from ImageView drawable
+		Drawable drawable = imageView.getDrawable();
+		Bitmap bmp = null;
+		if (drawable instanceof BitmapDrawable) {
+			bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+		} else {
+			return null;
+		}
+		// Store image to default external storage directory
+		Uri bmpUri = null;
+		try {
+			File file = new File(
+					Environment
+							.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+					"share_image_" + System.currentTimeMillis() + ".png");
+			file.getParentFile().mkdirs();
+			FileOutputStream out = new FileOutputStream(file);
+			bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+			out.close();
+			bmpUri = Uri.fromFile(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return bmpUri;
+	}
+
 	public static Transformation getImageScaleTransformer(
 			final int targetWidth, final int targetHeight) {
 		return new Transformation() {
--- src/com/maryamq/imageclient/activities/ImageDisplayActivity.java
+++ src/com/maryamq/imageclient/activities/ImageDisplayActivity.java
@@ -1,53 +1,81 @@
 package com.maryamq.imageclient.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import com.maryamq.imageclient.R;
+import com.maryamq.imageclient.Utils;
 import com.maryamq.imageclient.model.ImageResult;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class ImageDisplayActivity extends Activity {
+
+	private ShareActionProvider miShareAction;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.item_image_result);
-		getActionBar().hide();
 
-		ImageView ivImage = (ImageView) this
-				.findViewById(R.id.ivImage);
-	
-		TextView tvTitle = (TextView)this.findViewById(R.id.tvTitle);
+		ImageView ivImage = (ImageView) this.findViewById(R.id.ivImage);
+
+		TextView tvTitle = (TextView) this.findViewById(R.id.tvTitle);
 		ImageResult result = (ImageResult) this.getIntent()
 				.getSerializableExtra(SearchActivity.IMAGE_RESULT);
-		Picasso.with(this).load(result.fullUrl).into(ivImage);
-		tvTitle.setText(Html.fromHtml(result.title)); 
+		Picasso.with(this).load(result.fullUrl).into(ivImage, new Callback() {
+
+			@Override
+			public void onError() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onSuccess() {
+				// TODO Auto-generated method stub
+				setupShareIntent();
+			}
+
+		});
+		tvTitle.setText(Html.fromHtml(result.title));
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.image_display, menu);
+		// Locate MenuItem with ShareActionProvider
+		MenuItem item = menu.findItem(R.id.menu_item_share);
+		// Fetch reference to the share action provider
+		miShareAction = (ShareActionProvider) item.getActionProvider();
+		// Return true to display menu
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	// Gets the image URI and setup the associated share intent to hook into the
+	// provider
+	public void setupShareIntent() {
+		// Fetch Bitmap Uri locally
+		ImageView ivImage = (ImageView) findViewById(R.id.ivImage);
+		Uri bmpUri = Utils.getLocalBitmapUri(ivImage); // see previous remote
+														// images section
+		// Create share intent as described above
+		Intent shareIntent = new Intent();
+		shareIntent.setAction(Intent.ACTION_SEND);
+		shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+		shareIntent.setType("image/*");
+		// Attach share event to the menu item provider
+		miShareAction.setShareIntent(shareIntent);
 	}
-	
-	
+
 }
